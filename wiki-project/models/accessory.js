f891c60b1cf59d248180106a9af6c363137712c9
Kevin Ingles <66982466+kji304@users.noreply.github.com>
2021-03-29 14:50:12 -0400

    Create accessory.js

---
+++ wiki-project/models/accessory.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Cube = require('./cube');
+
+const accessorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        },
+    description: {
+        type: String,
+        required: true,
+        minLength: 5
+        },
+    imageUrl: {
+        type: String,
+        required: true,
+        },
+
+    cubes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Cube'
+        }]
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory;
