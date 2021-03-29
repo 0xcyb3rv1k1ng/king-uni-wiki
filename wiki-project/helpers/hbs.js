cf42101c3ec386c03e5f062eede39654b21b85d5
Kevin Ingles <66982466+kji304@users.noreply.github.com>
2021-03-29 14:49:27 -0400

    Create hbs.js

---
+++ wiki-project/helpers/hbs.js
@@ -0,0 +1,9 @@
+var Handlebars = require('handlebars');
+
+Handlebars.registerHelper('substr', function(length, context, options) {
+    if ( context.length > length ) {
+     return context.substring(0, length) + "...";
+    } else {
+     return context;
+    }
+   });
